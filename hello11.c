e5b749a8ca87b7770c9b4d7bc040fc44118360f5
Sergey Bokarev <snbokarev@mail.ru>
2019-08-28 01:29:26 +0400

    Hello11 is added

---
+++ hello11.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main () {
+  printf ("BBBBBBBBBB\nB   BB   B\nB   BB   B\n B B  B B\n\n");
+
+  printf (" S    S S\nS   SS   S\nS   SS   S\n S S    S\n\n");
+
+  printf ("NNNNNNNNNN\n     NN\n   NN\nNNNNNNNNNN\n");
+
+
+
+
+
+
+
+
+
+}
