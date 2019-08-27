6132981b94497241e788241d7d93ced2e5e8b576
root <root@localhost>
2019-08-27 20:06:46 +0400

    Completed comparing logic

--- hello9.c
+++ hello9.c
@@ -10,18 +10,33 @@ int main   () {
   printf ("Enter 5 values\n");
   scanf ("%d%d%d%d%d", &v1,&v2,&v3,&v4,&v5);
   
-  if v1>v2 { g1 = v1 };
-  if v2>v1 { g1 = v2 };
-  if v3>v4 { g2 = v3 };
-  if v4>v3 { g2 = v4 };
-  if g1>g2 { g3 = g1 };
-  if g2>g1 { g3 = g2 };
-  if v5>g3 { gr = v5 };
-  if g3>g5 { gr = g3 };
+  if (v1>=v2) { g1 = v1; };
+  if (v2>v1) { g1 = v2; };
+  
+  if (v3>=v4) { g2 = v3; };
+  if (v4>v3) { g2 = v4; };
+  
+  if (g1>=g2) { g3 = g1; };
+  if (g2>g1) { g3 = g2; };
+  
+  if (v5>=g3) { gr = v5; };
+  if (g3>v5) { gr = g3; };
   
   printf ( "Greatest value is: %d\n", gr);
   
+  if (v1<=v2) { l1 = v1; };
+  if (v2<v1) { l1 = v2; };
   
+  if (v3<=v4) { l2 = v3; };
+  if (v4<v3) { l2 = v4; };
+  
+  if (l1<=l2)   { l3 = l1; };
+  if (l2<l1)   { l3 = l2; };
+  
+  if (v5<=l3)  { lr = v5; };
+  if (l3<v5)  { lr = l3; };
+  
+  printf ("Least value is: %d", lr);
   
   return 0;
 
