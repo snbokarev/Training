055fa593ffea2636e0fe3f7387c037433e08ebc4
Sergey Bokarev <sergey.bokarev@tech-corps.com>
2019-08-27 05:00:21 +0400

    Added integers to hello8

--- hello8.c
+++ hello8.c
@@ -3,8 +3,12 @@
 /* Starting main */
 int main() {
 
+  int int1;
+
+  scanf ("%d", &int1);
+
   printf ("Hi\n");
-  printf ("*****\n*   *\n*   *\n*   *\n*****\n");
+  printf ("*****\n*   *\n*   *\n*   *\n*****\n%d %d",int1,int1);
 
   return 0;
 
