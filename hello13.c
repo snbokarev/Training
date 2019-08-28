5d40bd761d7278d398349d9dcf214f61b6ca2bbf
root <root@localhost>
2019-08-28 22:36:33 +0400

    Hello13 is added.

---
+++ hello13.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* Chess desk */
+
+int main() {
+	
+	printf (" ********\n");
+	printf ("********\n");
+	printf (" ********\n");
+	printf ("********\n");
+	printf (" ********\n");
+	printf ("********\n");
+	printf (" ********\n");
+	printf ("********\n");
+	
+	
+	return 0;
+	
+	
+	
+	
+}
