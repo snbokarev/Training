8945275061a7c3703416749d1b244e7cac8d8303
root <root@localhost>
2019-08-29 09:02:08 +0400

    Hell14 is added.

---
+++ hello14.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* Dispaying of numeric codes of literals*/
+int main () {
+	
+	printf ("%d %d %d %d %d %d ", 'A', 'B', 'C', 'a', 'b', 'c');
+	printf ("%d %d %d %d %d ", '$', '*', '+', '/', ' ');
+	
+	return 0;
+	
+	
+	
+}
