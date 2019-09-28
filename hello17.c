6583c7a645003c37c558c37b4a59cdbea0bfe6bd
root <root@localhost>
2019-09-28 09:28:10 +0400

    Complete 17

--- hello17.c
+++ hello17.c
@@ -9,15 +9,32 @@ int main () {
 	int fail = 0;
 	int stud = 1;
 	
-	while (stud < =10) {
+	printf ("Enter values\n");
 	
-		printf ("Enter %d result (1 or 2): ", stud);
+	while (stud <= 10)  {
+	
+		printf ("Enter %d result (1 or 2): \n ", stud);
 		scanf ( "%d",  &res);
 		
+		if (res ==1) {
+			pass = pass + 1;	
+		}
+		    else         {
+		    fail = fail + 1;	
+	    }
+		    	
+		stud = stud + 1;
+		
+     }
+	 
+	 printf ("\nPassed: %d\n", pass);
+	 printf ("Failed: %d\n\n", fail);
+	 
+	if (pass > 8) {
+	    
+	    printf ("Bonus teacher");	
 		
 	}
-	
-	
 	
 	
 
