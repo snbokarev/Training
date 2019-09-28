e60866fa81ac9cc59b732213082f6640042886f3
root <root@localhost>
2019-09-28 09:00:17 +0400

    new 17

---
+++ hello17.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* Lets start */
+
+int main () {
+	
+	int res;
+	int pass = 0;
+	int fail = 0;
+	int stud = 1;
+	
+	while (stud < =10) {
+	
+		printf ("Enter %d result (1 or 2): ", stud);
+		scanf ( "%d",  &res);
+		
+		
+	}
+	
+	
+	
+	
+
+
+}
