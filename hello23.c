b1884a8ecb75ea6a40344f78b3cd0318c7e125d9
root <root@localhost>
2019-10-25 08:56:19 +0400

    hello23 is created

---
+++ hello23.c
@@ -0,0 +1,32 @@
+/* Program for exercise 3.19 */
+
+/* It needs to obtain sales value and calculate
+employee's salary */
+
+
+
+int main () {
+	
+	float sales ;
+	
+	puts ("Enter sales value ( -1 to quit )") ;
+	scanf ("%f" , &sales);
+	
+	if ( sales >= 0 )  {
+	
+	   while ( sales >= 0 ) {
+	   	sales=sales * 0.09 + 200;
+	   	printf ("\nEmployee's salary is: %.2f\n\n" , sales);
+	   	puts ("Enter sales value ( -1 to quit )") ;
+	       scanf ("%f" , &sales);
+		
+	   } // End of while
+		
+		
+ 	} //  End of if
+	 else
+	 puts ("No values");
+	 
+	 puts ("Program is over");
+	
+} // End of main
