21a750cd414aed47c78817a748d8ccd7fc4355cc
root <root@localhost>
2019-10-28 04:52:38 +0400

    ex3.23 added

---
+++ ex3.23.c
@@ -0,0 +1,26 @@
+/* Program for exercise 3.23 */
+
+/* Find maximum from 10 vales input */
+
+/* Input value, assign it to largest, use cycle to input values and compare it to largest. If current value is greater than largest, then addign the value to largest. Output largest. */
+
+#include <stdio.h>
+
+int main () {
+   int counter , current , largest;
+   puts ("Enter 1 value: ");
+   scanf ("%d" , &largest);
+   counter = 2;
+   while ( counter <= 10 ) {
+   	printf ("Enter %d value: \n" , counter);
+   	scanf ("%d" , &current);
+   	if ( current > largest )
+   	largest = current;
+   	++counter;
+   } // End of While
+
+   printf ("\nThe maximum value is: %d" , largest);
+	
+	
+   return 0;
+} // End of main
