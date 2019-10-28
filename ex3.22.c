c43e59c2a4aa7640c7ba695ac05f80346cbb2d84
root <root@localhost>
2019-10-28 04:28:19 +0400

    ex3.22 added

---
+++ ex3.22.c
@@ -0,0 +1,14 @@
+/* Program for exercise 3.22 */
+
+/* Output numbers from 1 to 10 along a string, delimeted by 3 spaces */
+
+#include <stdio.h>
+int main () {
+	int i = 1;
+	while (  i <= 10 ) {
+	   printf ("%d   " , i);
+	   ++i;
+	} 
+	
+	
+}  // End of main
