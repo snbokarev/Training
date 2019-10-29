d6f950bb64f731de52bef94cb012c6073d86a7ab
Sergey Bokarev <snbokarev@mail.ru>
2019-10-29 04:42:36 +0400

    ex3.27 modified

--- ex3.27.c
+++ ex3.27.c
@@ -7,20 +7,26 @@
 #include <stdio.h>
 
 int main () {
-   int counter , current , largest;
+   int counter , current , largest1 , largest2;
+   
    puts ("Enter 1 value: ");
-   scanf ("%d" , &largest);
-   counter = 2;
+   scanf ("%d" , &largest1);
+   puts ("Enter 2 value: ");
+   scanf ("%d" , &largest2);
+   counter = 3;
+   
    while ( counter <= 10 ) {
-   	printf ("Enter %d value: \n" , counter);
-   	scanf ("%d" , &current);
-   	if ( current > largest )
-   	largest = current;
-   	++counter;
+   	  printf ("Enter %d value: \n" , counter);
+   	  scanf ("%d" , &current);
+   	  if ( current > largest1 )
+         largest1 = current;
+      if ( current < largest1 && current > largest2)
+         largest2 = current;
+   	  ++counter;
    } // End of While
 
-   printf ("\nThe maximum value is: %d" , largest);
-	
+   printf ("\nThe 1st maximum value is: %d" , largest1);
+   printf ("\nThe 2ns maximum value is: %d" , largest2);
 	
    return 0;
 } // End of main
