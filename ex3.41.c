5ef54c9409c9ea9d53112a3921912b57d770c803
root <root@localhost>
2019-11-09 15:21:18 +0400

    added 3.41

---
+++ ex3.41.c
@@ -0,0 +1,18 @@
+/* Program for exercise 3.41 */
+
+/* It needs to output powers of 2 infinitely */
+
+#include <stdio.h>
+
+int main () {
+  unsigned int i = 1;
+   while (i > 0) {
+      i *= 2;
+      printf ("%d \n" , i);	
+   	
+   	
+   }
+	
+	
+   return 0;	
+} // End of main
