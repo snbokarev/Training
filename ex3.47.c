e1cf9e8a30b2055888f7911de39da32259e87ec0
root <root@localhost>
2019-11-19 08:52:38 +0400

    3.47 modified

--- ex3.47.c
+++ ex3.47.c
@@ -6,6 +6,7 @@
 
 int main () {
    int val , fact = 1, n;
+   float e = 1;
    
    printf ("Enter your value\n");
    scanf ("%d" , &val);
@@ -19,17 +20,23 @@ int main () {
 	
    if (val == 0) {
       printf ("Factorial is: 1\n");
-      printf ("");
-   }
+    }
    else   {
       n = val;	
-      while ( n > 0 )   { // While 2
-      	fact *= n;
-      	e = e
-      	--n;    	
-      } // End of While 2
-      printf ("Factorial is: %d\n", fact);
-   	
+      while ( n > 0 )   { // While 3
+        while ( n > 0 )   { // While 2
+         	fact *= n;
+         	--n;    	
+          } // End of While 2
+         printf ("Factorial is: %d\n", fact);
+         e += (double) 1/fact;
+         --val;
+         n = val;
+         fact = 1;
+        } // End of While 3
+        
+        printf ("E is %.4f\n" , e);
+        
    } // End of if
     
 	
