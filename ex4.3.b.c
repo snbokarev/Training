4be1c95d66c05434b37facfb7f2514ed93f8b318
root <root@localhost>
2020-01-15 20:38:06 +0400

    4.3.b added

---
+++ ex4.3.b.c
@@ -0,0 +1,17 @@
+/* Program for 4.3.b */
+
+/* It needs to output float number in different manners */
+
+#include <stdio.h>
+
+int main () {
+	 
+	 float i = 333.546372;
+	 
+	 printf ("%15.1f\n" , i) ;
+	 printf ("%.2f\n" , i) ;
+	 printf ("%15.3f\n" , i) ;
+	 printf ("%.4f\n" , i) ;
+	
+	
+} // End of main
