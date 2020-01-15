241dc01a42311ef9e5129f7400a9797884292f8e
root <root@localhost>
2020-01-15 17:46:08 +0400

    4.3.a smart modified

--- ex4.3.a.c
+++ ex4.3.a.c
@@ -5,19 +5,27 @@
 
 int main () {
 	
-	int sum = 0;
+	int sum1 = 0 , sum2 = 0 , sumt = 0;
 	
-	for (int i = 1 ; i <= 99 ; ++i) {
-		if ( i % 2 == 0 ) {
-			
-			sum += i;
-		   //printf ("%d\n" , i);	
-		}
-	
+	// Sum of odd numbers
+	for (int i = 1 ; i <= 99 ; i += 2) {
+	   sum1 += i;
 	}  // End of for
 	
-	printf ("Sum of even numbers is: %d\n" , sum);
-		
+	// Sum of even numbers
+	for (int i = 2 ; i <= 99 ; i += 2) {
+	   sum2 += i;
+	}  // End of for
+	
+	// Total sum
+	for (int i = 1 ; i <= 99 ; ++i) {
+	   sumt += i;
+	}  // End of for
+	
+	printf ("Sum of odd numbers is: %d\n" , sum1);
+	
+	printf ("Sum of even numbers is: %d\n" , sum2);
+	printf ("Total sum of numbers is: %d\n" , sumt);	
 	
 	
 } // End of main
