9d0e651f842b4c008838e9f69350f994f65645de
root <root@localhost>
2020-01-22 00:44:58 +0400

    4.3.c added

---
+++ ex4.3.c.c
@@ -0,0 +1,16 @@
+/* Program for 4.3.c */
+
+/* It needs to output power 3 of 2.5 */
+
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+	float rez ;
+	rez = pow (2.5 , 3);
+	
+	printf ("Result is %-10.2f\n" , rez);
+	
+	
+	
+} // End of main
