bd27e28ea4f5da73700725ced74c60837b253c78
root <root@localhost>
2020-01-25 01:11:56 +0400

    4.3.e added

---
+++ ex4.3.e.c
@@ -0,0 +1,17 @@
+/* Program for 4.3.e */
+
+/* It needs to output numbers from 1 till 20, 5 numbers per line using for */
+
+int main () {
+	
+	for (int i = 1; i <= 20 ; i++) {
+		if (i % 5 == 0)
+		   printf ("%d\n" , i );
+	    else
+		   printf ("%d\t" , i );
+	} // End of for
+	
+	
+	
+	
+} // End of main
