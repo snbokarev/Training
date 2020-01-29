4b7c2a43dab677e7c4e4109245df8f10135053cb
root <root@localhost>
2020-01-30 02:27:50 +0400

    4.8 added

---
+++ ex4.8.c
@@ -0,0 +1,24 @@
+/* Program for 4.8 */
+
+#include <stdio.h>
+
+int main () {
+	int i , j , x , y;
+    puts ("Enter 1-20");
+    scanf ("%d%d" , &x , &y);
+    
+    for ( i= 1; i <= y ; i++) {
+    	for (j = 1; j <= x; j++)
+    	   printf ("@");
+        printf ("\n");
+    }	
+    	
+    	
+	
+	
+	
+	
+	
+	
+	
+	}//End of main
