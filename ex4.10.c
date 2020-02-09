ec1d987ed5de1d98eb1ab28c3116a216597c10ac
root <root@localhost>
2020-02-10 02:49:10 +0400

    4.10 modified

--- ex4.10.c
+++ ex4.10.c
@@ -2,3 +2,20 @@
 
 /* It needs to read integers untill 9999 is entered, then calculate its average value */
 
+#include <stdio.h>
+
+int main () {
+	int val = 0 , sum = 0 , i = 0;
+	
+	while (val != 9999) {
+		scanf ("%d" , &val);
+		sum += val;
+		i++;
+	} // End of while
+	
+	if (sum != 9999)
+	   printf ("Avg is %.2f\n" , (float)(sum-9999)/(i-1));
+	else
+	   puts ("No input");
+	
+} // End of main
