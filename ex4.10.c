87c41cb7ec79aab395e6288a64c50df48d41d03e
root <root@localhost>
2020-02-10 02:26:52 +0400

    added ex4.10

--- ex4.10.c
+++ ex4.10.c
@@ -1,23 +1,4 @@
 /* Program for 4.10 */
 
-/* It needs to read numbers until 9999 is entered and calculate their average value*/
+/* It needs to read integers untill 9999 is entered, then calculate its average value */
 
-#include <stdio.h>
-
-int main () {
-	int i, val, sum = 0;
-	
-    for (i = 1; i >= 1; i++) {
-    	scanf ("%d" , &val);
-    	if (val == 9999)
-    	   break;
-    	sum += val;
-    }
-	
-	if (i == 1)
-	   printf ("No input");
-    else
-   	printf ("Average is: %.1f\n" , (float) sum/(i-1));
-	
-	
-} // End of main
