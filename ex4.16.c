03c43c65e368d0908efeaa7894f22177b2a1aaf2
root <root@localhost>
2020-02-12 11:11:19 +0400

    modified 4.16

--- ex4.16.c
+++ ex4.16.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 
 int main () {
+	
+	
 	// Block A
 	for (int i = 1; i <= 10 ; i++) {
 	   for (int j = 1 ; j <= i ; j++)
@@ -22,6 +24,28 @@ int main () {
 	} // End of for with i
 	 printf("\n");
 	// End of block B
+
+		// Block C
+	for (int i = 10; i >= 1; i--) {
+	   for (int j = 1; j <= (10 - i ); j++)
+	      printf ("%s" , " ");
+	   for (int k = 1; k <= i; k++)
+	      printf ("%s" , "*");
+       printf("\n");
+	} // End of for with i
+	 printf("\n");
+	// End of block C 
+	
+		// Block D
+	for (int i = 1; i <= 10; i++) {
+	   for (int j = 1; j <= (10 - i ); j++)
+	      printf ("%s" , " ");
+	   for (int k = 1; k <= i; k++)
+	      printf ("%s" , "*");
+       printf("\n");
+	} // End of for with i
+	 printf("\n");
+	// End of block D
 	
 	
 } // End of main
