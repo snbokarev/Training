cc8d16de9b09f5924b061a4370ed4cf1d8f71de1
root <root@localhost>
2020-02-17 06:34:55 +0400

    added 4.17

---
+++ ex4.17.c
@@ -0,0 +1,3 @@
+/* Program for 4.17 */
+/* For each company we know account number, credit limit, balance. It needs to calculate new credit limit and output clients whos balance exceeds the new limit. */
+
