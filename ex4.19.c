189e54005b1485dc8c8026411f45a73c86a7359b
root <root@localhost>
2020-03-02 17:49:43 +0400

    4.19 modified

--- ex4.19.c
+++ ex4.19.c
@@ -5,46 +5,49 @@
 #include <stdio.h>
 
 int main () {
-	int num , q, i = 0;
+	int num = 0 , q, i = 0;
 	float sum=0.00;
-	printf ("%s\n" , "Enter 5 pairs: detail number and quantity");
-	while (++i <= 5) { // Main while
+	printf ("%s\n" , "Enter pairs: detail number and quantity. To quit enter -1 as detail number");
+	while (num >= 0) { // Main while
 	   scanf ("%d" , &num);
-	   scanf ("%d" , &q);
-	      
-	   while (num < 1 || num> 5) { // Checking while
-	      printf ("%d is out of scope, enter pair again\n" , num);	
-	      scanf ("%d" , &num);
+	   if (num != -1)
 	      scanf ("%d" , &q);
-	   } // End of checking while
 	   
-	   switch (num) {
-	   	case 1:
-	   	   sum += q*2.52;
-	      	break;
+	   if (num != -1) { 
+   	   while (num < 1 || num> 5) { // Checking while
+	         printf ("%d is out of scope, enter pair again\n" , num);	
+   	      scanf ("%d" , &num);
+	         scanf ("%d" , &q);
+   	   } // End of checking while
+	   
+	      switch (num) {
+	      	case 1:
+	      	   sum += q*2.52;
+	         	break;
 	      	
-	       case 2:
-	          sum += q*2.54;
-	      	break;
+   	       case 2:
+	             sum += q*2.54;
+   	      	break;
 	      	
-	      case 3:
-	          sum += q*2.55;
-	      	break;
+   	      case 3:
+	             sum += q*2.55;
+   	      	break;
 	      	
-	      case 4:
-	          sum += q*2.57;
-	      	break;
+   	      case 4:
+	             sum += q*2.57;
+	         	break;
 	      	
-	      case 5:
-	          sum += q*2.58;
-	      	break;
+	         case 5:
+   	          sum += q*2.58;
+	         	break;
 	      	
-	      default:
-	          printf ("%s\n" , "Wrong input, try again");
-	          break;
+	         default:
+	             printf ("%s\n" , "Wrong input, try again");
+	             break;
 	   	
 	   	
-	   } // End of switch
+	      } // End of switch
+       } // End of if	      
 		
 	} // End of Main while
 	
