60b3b286d60c1edc525af9e5bce29fa29518a039
root <root@localhost>
2020-04-14 23:19:54 +0400

    _5.3 added

---
+++ ex_5.3.c
@@ -0,0 +1,23 @@
+/* Program from figure 5.3 */
+
+/* It need to calculate squares of numbers from 1 till 10*/
+
+#include <stdio.h>
+
+int square(int); // Declare prototype
+
+
+int main () { // Main function
+
+   for (int i = 1; i <= 10 ; i++) {
+   	printf (" %d\n" , square (i));
+   } // End of for
+   
+   return 0;
+		
+} // End of main
+
+/* Definition of function */
+int square (int y) {
+	return y*y;
+}
