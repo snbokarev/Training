fc8de107481457f635de4e6d1cded8ad3603710a
root <root@localhost>
2020-05-02 08:35:00 +0400

    _5.15 modified

--- ex_5.15.c
+++ ex_5.15.c
@@ -3,13 +3,17 @@
 
 #include <stdio.h>
 
-long fibonacci (long); // Peototype of fibonacci function
+long fibonacci (long); // Prototype of fibonacci function
+long fibonacci2 (long); // Protorype of fibonacci2 function
 
 main () {
 	int f;
-	printf ("%s\n" , "Enter position of Fibonacci number");
+	printf ("%s\n" , "Enter position of Fibonacci number, calculating recursively");
 	scanf ("%d" , &f);
 	printf ("Fibonacci number for %d is %ld\n" , f , fibonacci (f));
+	printf ("\n%s\n" , "Enter position of Fibonacci number, calculating iteratively");
+	scanf ("%d" , &f);
+	printf ("Fibonacci number for %d is %ld\n" , f , fibonacci2 (f));
 	
    return 0;
 } // End of main
@@ -21,3 +25,18 @@ long fibonacci (long number) {
 	else
 	   return (fibonacci(number - 2) + fibonacci (number - 1));
 } // End of fibonacci function definition
+
+/* Definition for fibonacci2 function */
+long fibonacci2 (long number) {
+	int a = 0 , b = 1 , c = 0;
+	if (number == 0 || number == 1)
+	   return number;
+	else {
+		for (int i = 1 ; i <= number - 1 ; ++i) {
+			c =  a + b;
+			a = b;
+			b = c;
+		} // End of if	
+		return c;	
+	} // End of else
+} // End of fibonacci2 function definition
