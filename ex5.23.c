90ec569df42ac630f7d9b26e41cefdf44c25bce0
root <root@localhost>
2020-05-31 02:45:22 +0400

    5.23 modified

--- ex5.23.c
+++ ex5.23.c
@@ -15,6 +15,10 @@ main () {
 	printf ("%s\n" , "Enter time of second event n format hh mm ss");
 	scanf ("%d%d%d" , &h2 , &m2 , &s2);
 	
+	if ( (h1 < 12 && h2 >= 12) || (h1 >= 12 && h2 < 12)) {
+		printf ("%s\n" , "This is different halfs of day");
+	} else {
+	
     delta = abs (seconds (h1 , m1 , s1) - seconds (h2 , m2 , s2));
     
     hr = delta/3600;
@@ -22,7 +26,9 @@ main () {
     se = delta%3600%60;
     
     printf ("Time between the events: %d hours %d minutes %d seconds\n" , hr , mi , se);
+	} // End of else
 	
+	return 0;
 } // End of main
 
 int seconds(int hrs ,  int min , int sec) 
