b71f9f3992de95ad6fba24c63ba2cfe7173fdf7c
root <root@localhost>
2020-05-31 02:18:15 +0400

    5.23 added

---
+++ ex5.23.c
@@ -0,0 +1,35 @@
+/* Program for exercise 5.23 */
+/* It needs to create function which gets hrs, minutes, seconds and returnes seconds from 12 */
+
+#include <stdio.h>
+#include <math.h>
+
+int seconds(int, int , int); // Prototype
+
+main () {
+	int h1 , m1 , s1 , h2 , m2 , s2 , delta , hr , mi , se;
+	
+	printf ("%s\n" , "Enter time of first event n format hh mm ss");
+	scanf ("%d%d%d" , &h1 , &m1 , &s1);
+	
+	printf ("%s\n" , "Enter time of second event n format hh mm ss");
+	scanf ("%d%d%d" , &h2 , &m2 , &s2);
+	
+    delta = abs (seconds (h1 , m1 , s1) - seconds (h2 , m2 , s2));
+    
+    hr = delta/3600;
+    mi = delta%3600/60;
+    se = delta%3600%60;
+    
+    printf ("Time between the events: %d hours %d minutes %d seconds\n" , hr , mi , se);
+	
+} // End of main
+
+int seconds(int hrs ,  int min , int sec) 
+{
+	if (hrs >= 12)
+	   hrs -= 12;
+	   
+	return hrs*3600 + min*60 + sec;
+	
+} // End of definition seconds
