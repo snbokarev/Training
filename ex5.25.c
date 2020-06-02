23a56baa6a22c00c74c85d320ce185b6068f6b25
root <root@localhost>
2020-06-02 07:15:23 +0400

    5.25 modified

--- ex5.25.c
+++ ex5.25.c
@@ -8,7 +8,26 @@ float min(float , float , float); // Prototype
 main ()
 {
 	
+	float val1 , val2 , val3;
+	printf ("%s\n" , "Enter 3 float values.");
+	scanf ("%f%f%f" , &val1 , &val2 , &val3);
+	
+	printf ("The minimum is: %.3f\n" , min (val1 , val2 , val3));
 	
 	return 0;
 	
 } // End of main
+
+float min(float x, float y , float z)
+{
+	float m;
+	m = x;
+	if (y <= x)
+	   m = y;
+	if (z <= x && z <= y)
+	   m = z;
+	 
+	return m;
+	
+	
+} // End of min
