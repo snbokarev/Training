6ba77dbc61da079518dbac70455ff3abcd393f51
root <root@localhost>
2020-06-02 07:02:53 +0400

    5.25 added

---
+++ ex5.25.c
@@ -0,0 +1,14 @@
+/* Program for exercise 5.25 */
+/* Find minimum of 3 float numbers */
+
+#include <stdio.h>
+
+float min(float , float , float); // Prototype
+
+main ()
+{
+	
+	
+	return 0;
+	
+} // End of main
