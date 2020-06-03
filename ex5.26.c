5336a6d27ddbd360f8ad385e15e6e3f8df2c093e
root <root@localhost>
2020-06-03 06:50:41 +0400

    5.26 modified

--- ex5.26.c
+++ ex5.26.c
@@ -7,23 +7,41 @@ int perfect (int); // Prototype
 
 main () 
 {
-	int sum , max = 100;
+	int max;
+	
+	printf ("%s\n" , "Enter maximum value");
+	scanf ("%d" , &max);
 
 	for (int i = 1; i <= max; ++i)
 	{
-		sum = 0;
-		printf ("%d" , i);
-		for (int j = 1; j < i; ++j)
-		{
-			if (i % j == 0)
-			   sum += j;
-		} // end of for j
-		if (sum == i)
-		   printf ("%s\n" , " is perfect");
-		else
-		   printf ("%s\n" , " is not perfect");
-		
+		if (perfect(i) == 1) {
+		   printf ("%d is perfect number. Dividers are: " , i);	
+		   for (int k = 1; k < i; ++k)
+		   {
+		   	if (i % k == 0)
+		   	   printf ("%d " , k);  	
+		   } // end of for k
+		puts ("");
+		} // end of if
 	} // end of for i
 	
+	printf ("%s\n" , "Done!");
 	
+	return 0;
 } // End of main
+
+
+int perfect (int val) // Definition of perfect
+{
+	int sum = 0;
+	for (int j = 1; j < val; ++j)
+		{
+			if (val % j == 0)
+			   sum += j;
+		} // end of for j
+	if (sum == val)
+	   return 1;
+	else
+	   return 0;
+	
+} // End of perfect
