dc0fef3b7015dcdfb05accc9297cea2f7630ec72
root <root@localhost>
2020-06-22 02:47:25 +0400

    5.29 modified

--- ex5.29.c
+++ ex5.29.c
@@ -7,33 +7,36 @@ int gcd(int, int) ; // Prototype of gcd
 
 main()
 {
+	int val1, val2;
+	printf ("%s\n" , "Enter 2 values");
+	scanf ("%d%d" , &val1 , &val2);
 	
+	printf ("The grand common divider is: %d\n" , gcd (val1 , val2));
+	
+	return 0;
 	
 } // End of main
 
 int gcd(int x, int y) // definition of gcd
 {
-	int great, less;
+	int great, less , buf;
 	   
     if (x >= y)
     {
     	great = x;
     	less = y;
-    }
-    
-    if (y >= x)
-    {
+    } else {
     	great = y;
     	less = x;
     }
-	
+    
 	while (great%less != 0)
-	{
-		buf = great;
-	    great = less;
-		less = buf % less;
-			
-	} // end of while
-		
+    {
+	   buf = great;
+	   great = less;
+	   less = buf % less;
+ 	} // end of while
 	
+	return less;
+		
 } // End of definition
