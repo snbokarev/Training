e2a3d962e44fdeb0dea7b573180aeb15630ae32c
root <root@localhost>
2020-09-10 03:05:08 +0400

    5.47 added

---
+++ ex5.47.c
@@ -0,0 +1,27 @@
+/* Program for the exercise 5.47 */
+/* It needs to understand what the program does */
+
+#include <stdio.h>
+
+int mystery(int, int);
+
+main()
+{
+	int x, y;
+	printf("%s\n", "Enter two integers: ");
+	scanf("%d%d", &x, &y);
+	
+	printf("The result is: %d\n", mystery(x, y));
+	
+	return 0;
+	
+} // End of main
+
+int mystery(int a, int b)
+{
+	if (b == 1)
+	    return a;
+	else
+	    return a + mystery(a, b - 1);
+	
+} // End of mystery
