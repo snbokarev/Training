d5db17df30260156e7281de5aafbd3d1f26a4996
root <root@localhost>
2020-09-22 13:52:45 +0400

    _6.7 added

---
+++ ex_6.7.c
@@ -0,0 +1,17 @@
+/* Program from figure 6.7 */
+
+#include <stdio.h>
+#define RESPONSE_SIZE 40
+#define FREQUENCY_SIZE 11
+
+main()
+{
+	int answer, raiting;
+	int response[RESPONSE_SIZE] = {1, 2, 3 , 4 , 5 , 6, 7, 8, 9, 10, 1, 2 , 3 , 4 , 5 ,6, 7, 7 , 8 , 8, 5, 5, 6, 6, 7, 7, 7, 8, 2, 3, 5, 6, 6, 7, 7, 7, 5, 5};
+	int frequency[FREQUENCY_SIZE] = {0};
+	
+	
+	
+	
+	
+} // End of main
