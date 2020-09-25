656e5f70e2f653a5e1032cc7aa34098ab73f8268
root <root@localhost>
2020-09-25 07:12:35 +0400

    _6.8 added

---
+++ ex_6.8.c
@@ -0,0 +1,17 @@
+/* Program from figure 6.8 */
+/* Printing histogram */
+
+#include <stdio.h>
+#define SIZE 10
+
+main()
+{
+	int n[SIZE] = {15, 1, 23, 8, 5, 10, 11, 9, 17, 2};
+	int i, j;
+	
+	printf("%s%13s%17s\n", "Element", "Value", "Histogram");
+	
+	
+	return 0;
+	
+} //End of main
