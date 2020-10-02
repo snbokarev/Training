5adf7c4d7973b1dfe58900027c0ec7d50049a9bf
root <root@localhost>
2020-10-02 07:24:50 +0400

    _6.15 added

--- ex_6.15.c
+++ ex_6.15.c
@@ -1,23 +1,31 @@
-/* Program fron figure 6.15 */
-/* Const demonstration */
+/* Program from figure 6.15 */
+/* Array bubble sorting */
 
 #include <stdio.h>
-
-void tryToModifyArray(const int []);
+#define SIZE 10
 
 main()
 {
-	int a[3] = {10, 20, 30};
-	tryToModifyArray(a);
-	printf("%d %d %d\n", a[0], a[1], a[2]);
-	
-	return 0;
-	
+    int a[SIZE] = {5, 10, 1, 3, 7, 8, 2, 4, 55, 0};
+    int i, pass, hold;
+    
+    printf("%s\n", "The original array is:");
+    for (i = 0; i <= SIZE - 1; i++)
+        printf("%4d", a[i]);
+        
+    // Sorting itself
+    for (pass = 1; pass <= SIZE - 1; pass++)
+        for (i = 0; i <= SIZE - 2; i++) 
+        	if (a[i] > a[i + 1]) {
+        		hold = a[i];
+        		a[i] = a[i + 1];
+        		a[i + 1] = hold;
+        	} // end of if
+    
+    printf("\n%s\n", "The sorted array is:");
+    for (i = 0; i <= SIZE - 1; i++)
+        printf("%4d", a[i]);
+        
+    return 0;
+        
 } // End of main
-
-void tryToModifyArray(const int b[])
-{
-	b[0] /= 2;
-	b[1] /= 2;
-	b[2] /= 2;
-}
