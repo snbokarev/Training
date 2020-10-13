001a808f308f76cc8f59049c7c4d4a4e8f2a0168
root <root@localhost>
2020-10-14 02:04:48 +0400

    6.4 modified

--- ex6.4.c
+++ ex6.4.c
@@ -5,6 +5,13 @@
 
 main()
 {
+	int table[SIZE][SIZE];
+	int x, y;
 	
+	for (x = 0; x <= SIZE - 1; x++)
+	    for (y = 0; y <= SIZE - 1; y++) {
+	        table[x][y] = x + y;
+	        printf("%3d\n", table[x][y]);
+	    }
 	
 } // End of main 
